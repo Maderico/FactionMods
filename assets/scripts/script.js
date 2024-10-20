5a2be37f68372e777dee73a70e51c07b727addc6
Alejandro Pelaez <112983858+Maderico@users.noreply.github.com>
2024-10-20 02:35:30 +0200

    Added FactionMods

---
+++ assets/scripts/script.js
@@ -0,0 +1,47 @@
+var modpackURL = "https://tiktok.com/@faction.network";
+var tutorialURL = "https://youtu.be/_jbrxuqU-HQ";
+var discordURL = "https://discord.faction.es/";
+var tiendaURL = "https://tienda.faction.es/";
+
+document.querySelector(".modpack").addEventListener("click", function () {
+  window.open(modpackURL);
+  Swal.fire({
+    icon: "success",
+    title: "¡Has abierto el Mod!",
+    text: "Mira la otra pestaña.",
+    showCancelButton: true,
+    confirmButtonText: "OK",
+    cancelButtonText: "Tutorial",
+    reverseButtons: true,
+    customClass: {
+      cancelButton: "order-third",
+    },
+  }).then((result) => {
+    if (result.dismiss === Swal.DismissReason.cancel) {
+      window.open(tutorialURL);
+    }
+  });
+});
+
+document.getElementById("btnDiscord").addEventListener("click", function () {
+  window.open(discordURL);
+});
+
+document.getElementById("btnTienda").addEventListener("click", function () {
+  window.open(tiendaURL);
+});
+
+window.addEventListener("load", function () {
+    const loaderWrapper = document.querySelector('.loader-wrapper');
+    const content = document.querySelector('.content-wrapper');
+    
+    setTimeout(() => {
+        loaderWrapper.classList.add('fadeOut');
+        content.style.display = 'block';
+        document.body.style.overflow = 'auto';
+
+        loaderWrapper.addEventListener('animationend', () => {
+            loaderWrapper.style.display = 'none';
+        });
+    }, 1500);
+});
