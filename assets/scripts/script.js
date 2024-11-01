653865951372682d4c36785fc0d3e28e94a1f00f
Alejandro Pelaez <112983858+Maderico@users.noreply.github.com>
2024-11-01 21:04:57 +0100

    Update script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,4 @@
-var modpackURL = "https://tiktok.com/@faction.network";
+var modpackURL = "https://cdn.discordapp.com/attachments/1269073093310283959/1301999264699777024/DedSafio_3_-_Faction_Network.rar?ex=672684ce&is=6725334e&hm=cc2641c675e7f4684b60f0a954d8cf14f996443e839a9c5b9152e6e2a2079a76&";
 var tutorialURL = "https://youtu.be/_jbrxuqU-HQ";
 var discordURL = "https://discord.faction.es/";
 var tiendaURL = "https://tienda.faction.es/";
